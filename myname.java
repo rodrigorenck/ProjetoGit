7bc2429f81fc0fc9da75a0ec8cdfa1c703bd2722
Rodrigo Renck <renckrodrigo030@gmail.com>
2022-04-15 23:17:29 -0300

    removi parte do meu nome e adicionei uma frase

--- myname.java
+++ myname.java
@@ -1,5 +1,6 @@
 public class myname {
     public static void main(String[] args) {
-        System.out.println("My name is Rodrigo!");
+        System.out.println("My name is Rod!");
+        System.out.println("Alterando o arquivo, vou remover meu nome!");
     }
 }
